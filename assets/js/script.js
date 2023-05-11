b47da5581f57bce592948570c1a8f4f0896321bd
LarissaMarquesPimenta <laariipimenta@gmail.com>
2023-05-11 12:12:02 -0300

    atualização javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ let nome = document.getElementById('nome')
 
 let email = document.querySelector('#email')
 
+let assunto = document.querySelector('#assunto')
+
+
 function validaNome() {
 let txtNome = document.querySelector('#txtNome')
 console.log(nome.value);
@@ -17,6 +20,30 @@ if(nome.value.length < 2) {
 }
 
 }
+
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if (email.value.indexOf('@') == -1)
+    txtEmail.innerHTML = 'Email invalido!'
+    txtEmail.style.color = 'red'
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    console.log(nome.value);
+    if(assunto.value.length > 100) {
+        txtAssunto.innerHTML = 'Assunto muito longo, reduza para 100 caracteres!'
+        txtAssunto.style.color = 'red'
+        assunto.style.border='3px solid red'
+    } else {
+        txtAssunto.innerHTML = 'Assunto valido!'
+        txtAssunto.style.color = 'green'
+        assunto.style.border='3px solid green'
+    }
+    
+    }
 
 function olaMundo() {
     alert('Clicou no botão')
